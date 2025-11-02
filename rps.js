78e8ceb16b58f65e70ce844ca47e964b4d6aecd7
Andre Carvalhais <carvalhais@live.com>
2025-11-02 19:52:41 -0300

    feat: implement getComputerChoice()

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
-console.log("Hello World");
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    let randomChoice = null;
+
+    if (randomNumber < 1/3) {
+        randomChoice = "rock";
+    } else if (randomNumber < 2/3) {
+        randomChoice = "paper";
+    } else {
+        randomChoice = "scissors"
+    }
+
+    return randomChoice;
+}
