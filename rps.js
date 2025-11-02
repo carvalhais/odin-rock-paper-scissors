4f15f6590381ebaf97410b4a7075af3eef2a908c
Andre Carvalhais <carvalhais@live.com>
2025-11-02 20:38:05 -0300

    feat: implement playGame()

--- rps.js
+++ rps.js
@@ -1,6 +1,3 @@
-let computerScore = 0;
-let humanScore = 0;
-
 function getComputerChoice() {
     let randomNumber = Math.random();
     let randomChoice = null;
@@ -21,48 +18,64 @@ function getHumanChoice() {
     return choice.trim().toLowerCase();
 }
 
-function playRound(computerChoice, humanChoice) {
-    console.log(`Computer chose "${computerChoice}".`);
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
 
-    switch (computerChoice) {
-        case "rock":
-            if (humanChoice === "rock") {
-                console.log("Tie! Both players chose rock.");
-            }
-            else if (humanChoice === "paper") {
-                console.log("You win! Paper beats rock!");
-                humanScore++;
-            }
-            else {
-                console.log("You lose! Rock beats scissors!");
-                computerScore++;
-            }
-            break;
-        case "paper":
-            if (humanChoice === "rock") {
-                console.log("You lose! Paper beats rock!");
-                computerScore++;
-            }
-            else if (humanChoice === "paper") {
-                console.log("Tie! Both players chose rock.");
-            }
-            else {
-                console.log("You win! Scissors beats paper!");
-                humanScore++;
-            }
-            break;
-        case "scissors":
-            if (humanChoice === "rock") {
-                console.log("You win! Rock beats scissors!");
-                humanScore++;
-            }
-            else if (humanChoice === "paper") {
-                console.log("You lose! Scissors beats paper!");
-                computerScore++;
-            }
-            else {
-                console.log("Tie! Both players chose scissors.");
-            }
-            break;
+    function playRound(computerChoice, humanChoice) {
+        console.log(`Computer chose "${computerChoice}".`);
+        console.log(`Human chose "${humanChoice}".`);
+
+        switch (computerChoice) {
+            case "rock":
+                if (humanChoice === "rock") {
+                    console.log("Tie! Both players chose rock.");
+                }
+                else if (humanChoice === "paper") {
+                    console.log("You win! Paper beats rock!");
+                    humanScore++;
+                }
+                else {
+                    console.log("You lose! Rock beats scissors!");
+                    computerScore++;
+                }
+                break;
+            case "paper":
+                if (humanChoice === "rock") {
+                    console.log("You lose! Paper beats rock!");
+                    computerScore++;
+                }
+                else if (humanChoice === "paper") {
+                    console.log("Tie! Both players chose rock.");
+                }
+                else {
+                    console.log("You win! Scissors beats paper!");
+                    humanScore++;
+                }
+                break;
+            case "scissors":
+                if (humanChoice === "rock") {
+                    console.log("You win! Rock beats scissors!");
+                    humanScore++;
+                }
+                else if (humanChoice === "paper") {
+                    console.log("You lose! Scissors beats paper!");
+                    computerScore++;
+                }
+                else {
+                    console.log("Tie! Both players chose scissors.");
+                }
+                break;
+        }
     }
+
+    let i = 1;
+    while (i <= 5) {
+        console.log(`Playing round #${i}.`)
+        playRound(getComputerChoice(), getHumanChoice());
+        i++;
+    }
+
+    console.log(`Final computer score: ${computerScore}`);
+    console.log(`Final human score: ${humanScore}`);
 }
